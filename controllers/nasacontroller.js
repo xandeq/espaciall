3807534adf66b411c7948b2824ac6846549e069d
xandeq <xandeq@gmail.com>
2020-06-24 19:14:19 -0300

    teste

--- controllers/nasacontroller.js
+++ controllers/nasacontroller.js
@@ -1,46 +1,51 @@
-const Apod = require('../models/apod')
-const { model, collection } = require('../models/apod')
-const https = require('https')
-const mongoose = require('mongoose')
+const Apod = require("../models/apod");
+const { model, collection } = require("../models/apod");
+const https = require("https");
+const mongoose = require("mongoose");
 
 exports.getApods = (req, res) => {
-
-    const apods = Apod.find().select("copyrights explanation url")
-    .then(apods => {
-        res.json({apods})
+  const apods = Apod.find()
+    .select("copyrights explanation url")
+    .then((apods) => {
+      res.json({ apods });
     })
-    .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.createApod = (req, res) => {
-    const url = "https://api.nasa.gov/planetary/apod?api_key=BzCHxf3K4h1enx5cUDETYFuGaexAd9NaOib69ZKy";
+  const url =
+    "https://api.nasa.gov/planetary/apod?api_key=BzCHxf3K4h1enx5cUDETYFuGaexAd9NaOib69ZKy&date=2020-06-22";
 
+  https
+    .get(url, (resp) => {
+      let data = "";
 
-    https.get(url, (resp) => {
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
 
-        let data = ''
+      resp.on("end", () => {
+        const apod = new Apod(JSON.parse(data));
+        console.log(JSON.parse(data).url);
 
-        resp.on('data', (chunk) => {
-            data += chunk;
-          });
-
-        resp.on('end', () => {
-            const apod = new Apod(JSON.parse(data))
-            console.log(JSON.parse(data).url)
-
-            if(!collection.findOne({'url': JSON.parse(data).url}))
-            {
-                apod.save()
-                .then(result => res.status(200).json({
-                    apod: result
-                }))
+        mongoose.model("Apod").findOne({ url: JSON.parse(data).url }),
+          function (err, result) {
+            console.log("entrou aqui");
+            if (!result) {
+              console.log("nao existe");
+              apod.save().then((result) =>
+                res.status(200).json({
+                  apod: result,
+                })
+              );
+            } else {
+              res.send({ message: "Ja existe" });
             }
-            else {
-                res.send({'message': 'Ja existe.'})
-            }
-        })
-    }).on('error', (err) => {
-        console.log('Error ' + err.message)
+            if (err) console.log("Erro " + err);
+          };
+      });
     })
-}
-
+    .on("error", (err) => {
+      console.log("Error " + err.message);
+    });
+};
