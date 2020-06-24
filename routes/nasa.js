6af05a3446ad8cd75d3044e4033d98f750a33f57
xandeq <xandeq@gmail.com>
2020-06-24 06:28:17 -0300

    commit

--- routes/nasa.js
+++ routes/nasa.js
@@ -1,2 +1,9 @@
 const express = require('express')
+const { createApod, getApods } = require('../controllers/nasacontroller')
+
 const router = express.Router()
+
+router.getApods('/nasa/getapods', getApods)
+router.postApod('/nasa/postapod', createApod)
+
+module.exports = router
